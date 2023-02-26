38c1c62ce76619ace33ad65967a264c6b2d3e6f9
paulomarcelo <paulo.marcelo@gmail.com>
2023-02-26 18:27:35 -0300

    Remover o item da mochila apenas na interface

--- js/main.js
+++ js/main.js
@@ -48,13 +48,30 @@ function criaElemento(item) {
     const numeroItem = document.createElement('strong')
     numeroItem.innerHTML = item.quantidade
     numeroItem.dataset.id = item.id
-
     novoItem.appendChild(numeroItem)
+
     novoItem.innerHTML += item.nome
+
+    novoItem.appendChild(botaoDeleta())
 
     lista.appendChild(novoItem)
 }
 
 function atualizaElemento(item) {
     document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
+
+function botaoDeleta() {
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = "X"
+
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode)
+    })
+
+    return elementoBotao
+}
+
+function deletaElemento(tag) {
+    tag.remove()
 }
